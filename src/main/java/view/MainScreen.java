f1ab8813e8ab9ded0b06aa8675fbf2cc695cdfa1
ViniciusNeivas <viniciusneivas@outlook.com.br>
2023-01-06 15:41:56 -0300

    Validação do nome do projeto - onde sem nome não é permitido criar um projeto

--- src/main/java/view/MainScreen.java
+++ src/main/java/view/MainScreen.java
@@ -14,6 +14,7 @@ import java.util.List;
 import javax.swing.DefaultListModel;
 import model.Project;
 import model.Task;
+import util.ButtonColumnCellRenderer;
 import util.DeadlineColumnCellRenderer;
 import util.TaskTableModel;
 
@@ -461,6 +462,12 @@ public class MainScreen extends javax.swing.JFrame {
 
         jTableTasks.getColumnModel().getColumn(2)
                 .setCellRenderer(new DeadlineColumnCellRenderer());
+        
+        jTableTasks.getColumnModel().getColumn(4).
+                setCellRenderer((new ButtonColumnCellRenderer("edit")));
+        
+        jTableTasks.getColumnModel().getColumn(5).
+                setCellRenderer((new ButtonColumnCellRenderer("delete")));
         //Criando um sort automático para as colunas da table
         jTableTasks.setAutoCreateRowSorter(true);
     }
--- src/main/java/view/ProjectDialogScreen.java
+++ src/main/java/view/ProjectDialogScreen.java
@@ -148,15 +148,22 @@ public class ProjectDialogScreen extends javax.swing.JDialog {
     private void jLabelToolBarSaveMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabelToolBarSaveMouseClicked
         // TODO add your handling code here:
         try {
-            Project project = new Project();
-            project.setName(jTextFieldName.getText());
-            project.setDescription(jTextAreaDescription.getText());
-            controller.save(project);
-            JOptionPane.showMessageDialog(rootPane, "Projeto salvo com sucesso");
+            if (!jTextFieldName.getText().equals("")) {
+                Project project = new Project();
+                project.setName(jTextFieldName.getText());
+                project.setDescription(jTextAreaDescription.getText());
+                controller.save(project);
+                JOptionPane.showMessageDialog(rootPane, "Projeto salvo com sucesso");
+                this.dispose();
+            } else {
+                JOptionPane.showMessageDialog(rootPane, "O projeto não foi salvo, "
+                        + "pois o campo nome não foi preenchido.");
+            }
+
         } catch (Exception e) {
             JOptionPane.showConfirmDialog(rootPane, e.getMessage());
         }
-        this.dispose();
+        
     }//GEN-LAST:event_jLabelToolBarSaveMouseClicked
 
     /**
